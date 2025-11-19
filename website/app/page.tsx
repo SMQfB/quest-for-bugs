55c1c71b52f7124aacfec5dc2f333a977f711d1c
ampelectrecuted <197376797+ampelectrecuted@users.noreply.github.com>
2025-11-19 21:39:55 +0000

    i dont want clanker commit message but i added actual content to the website

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -4,8 +4,9 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Welcome to Quest for Bugs.
+            Scratch Movie: Quest for Bugs
           </h1>
+          <p class="mt-2">The Scratch Movie: Quest for Bugs is a movie directed by scratchcode1_2_3 and mali3000 that we created since we wanted to have an official Scratch Movie, but, unfortunately, that is not one of the priorities of the Scratch Team. So we created this!</p>
         </div>
       </main>
     </div>
