b5a42ef12e5238dda36e6b5615cbdbf232285054
Jeremiah N <jnwaokolo@icloud.com>
2025-11-20 18:25:36 +0000

    Update page.tsx

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             Scratch Movie: Quest for Bugs
           </h1>
-          <p class="mt-2">The Scratch Movie: Quest for Bugs is a movie directed by scratchcode1_2_3 and mali3000 that we created since we wanted to have an official Scratch Movie, but, unfortunately, that is not one of the priorities of the Scratch Team. So we created this!</p>
+          <p className="mt-2">The Scratch Movie: Quest for Bugs is a movie directed by scratchcode1_2_3 and mali3000 that we created since we wanted to have an official Scratch Movie, but, unfortunately, that is not one of the priorities of the Scratch Team. So we created this!</p>
         </div>
       </main>
     </div>
